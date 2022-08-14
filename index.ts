cea0295a5d884eca8c58e4ca04c32bf16f3e7d8e
Artur Stambultsian <bumbledos@gmail.com>
2022-08-14 20:35:58 +0400

    ThemeParams: changed params type

--- index.ts
+++ index.ts
@@ -29,13 +29,13 @@ export interface WebAppInitData {
 }
 
 export interface ThemeParams {
-  bg_color: string;
-  secondary_bg_color: string;
-  text_color: string;
-  hint_color: string;
-  link_color: string;
-  button_color: string;
-  button_text_color: string;
+  bg_color: `#${string}`;
+  secondary_bg_color: `#${string}`;
+  text_color: `#${string}`;
+  hint_color: `#${string}`;
+  link_color: `#${string}`;
+  button_color: `#${string}`;
+  button_text_color: `#${string}`;
 }
 
 export interface HapticFeedback {
