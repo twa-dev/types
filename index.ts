56868994806d17c5b0997c1fe87748405c87d1ce
Artur Stambultsian <bumbledos@gmail.com>
2022-08-15 22:17:45 +0400

    InvoiceStatuses: success => paid

--- index.ts
+++ index.ts
@@ -77,7 +77,7 @@ export interface MainButton {
   }) => void;
 }
 
-type InvoiceStatuses = "pending" | "failed" | "cancelled" | "success";
+type InvoiceStatuses = "pending" | "failed" | "cancelled" | "paid";
 
 export type EventNames =
   | "invoiceClosed"
