57b944fbbcc381359bdbaee54e74bf82f068a3af
Artur Stambultsian <bumbledos@gmail.com>
2024-01-09 19:37:05 +0700

    ThemeParams: added fields header_bg_color, accent_text_color, section_bg_color, section_header_text_color, subtitle_text_color, destructive_text_color

--- index.ts
+++ index.ts
@@ -41,6 +41,12 @@ export interface ThemeParams {
   link_color: `#${string}`;
   button_color: `#${string}`;
   button_text_color: `#${string}`;
+  header_bg_color: `#${string}`;
+  accent_text_color: `#${string}`;
+  section_bg_color: `#${string}`;
+  section_header_text_color: `#${string}`;
+  subtitle_text_color: `#${string}`;
+  destructive_text_color: `#${string}`;
 }
 
 export interface HapticFeedback {
